a207c3a82b31a6e2d2f0b92ea70fdf4058fc0c3d
Philip Yu <pyu3@mail.sfsu.edu>
2019-11-07 20:52:53 -0800

    User can see a counter with total number of characters left for tweet on compose tweet page

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ The following **required** functionality is completed:
   - [ ] User can then enter a new tweet and post this to twitter
   - [ ] User is taken back to home timeline with **new tweet visible** in timeline
   - [ ] Newly created tweet should be manually inserted into the timeline and not rely on a full refresh
-  - [ ] User can **see a counter with total number of characters left for tweet** on compose tweet page
+  - [X] User can **see a counter with total number of characters left for tweet** on compose tweet page
 
 #### OPTIONAL
 The following **optional** features are implemented:
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -119,8 +120,9 @@ public class TimelineActivity extends AppCompatActivity {
 
         if (item.getItemId() == R.id.compose) {
 //            Compose icon has been tapped
-            Toast.makeText(this, "Compose", Toast.LENGTH_SHORT).show();
 //            Navigate to the compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
             return true;
         }
 
