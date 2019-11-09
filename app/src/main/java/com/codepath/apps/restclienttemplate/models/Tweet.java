752500d896ee917cda1be7627497bca85e209bae
Philip Yu <pyu3@mail.sfsu.edu>
2019-11-08 20:02:21 -0800

    User can compose and post a new tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -3,16 +3,23 @@ package com.codepath.apps.restclienttemplate.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Tweet {
 
     public String body;
     public String createdAt;
     public long id;
     public User user;
+
+    // empty constructor needed by the Parceler libraryk
+    public Tweet() {
+
+    }
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
