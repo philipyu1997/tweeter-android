ad86fdcce57bd6877c7ec5de0d5787d96ee2ef61
Philip Yu <philipyu1997@gmail.com>
2020-05-24 22:16:13 -0700

    Persist tweets offline

---
+++ app/src/main/java/com/yuphilip/model/TweetWithUser.java
@@ -0,0 +1,4 @@
+package com.yuphilip.model;
+
+public class TweetWithUser {
+}
--- app/src/main/java/com/yuphilip/model/database/MyDatabase.java
+++ app/src/main/java/com/yuphilip/model/database/MyDatabase.java
@@ -3,10 +3,12 @@ package com.yuphilip.model;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {SampleModel.class}, version = 1)
+@Database(entities = {Tweet.class, User.class}, version = 2)
 public abstract class MyDatabase extends RoomDatabase {
+
+    public abstract TweetDao tweetDao();
+
     // Database name to be used
     public static final String NAME = "MyDataBase";
 
-    public abstract SampleModelDao sampleModelDao();
 }
